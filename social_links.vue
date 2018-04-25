802effded31a70934d373e1960b102b86541c44d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-25 17:38:03 -0400

    Parkland Mall 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <a href="https://www.instagram.com/parklandmall" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-        <a href="https://www.facebook.com/bonniedoonsc" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+        <a href="https://www.facebook.com/ParklandMall" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
         <!--<a href="https://www.pinterest.ca/bonniedoonsc" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>-->
         <a href="//codecloud.cdn.speedyrails.net/sites/5ae0d66d6e6f6412f32c0000/image/jpeg/1521054167714/28907698_10156615165667323_2001433750_n.jpg" data-lightbox="Snapchat" data-title="BCC Snapchat"> <i class="fa fa-snapchat-ghost"></i></a>
         <a href="https://www.twitter.com/bonniedoonsc" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
