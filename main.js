fa9387d7653476df6780375f156bed9f45b9e2fd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-26 10:27:04 -0400

    Parkland Mall 1.0  /main.js

--- main.js
+++ main.js
@@ -40,6 +40,7 @@ require.config({
         'vue-masonry-plugin' : 'https://cdn.jsdelivr.net/npm/vue-masonry@0.10.16/dist/vue-masonry-plugin.min',
         'vue-paginate': 'https://cdnjs.cloudflare.com/ajax/libs/vue-paginate/3.5.1/vue-paginate',
         'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext',
+        'bootstrap-vue' : '//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue',
         
     }
 });
