6764e4bc081346b3d2c6b341075435681fd6855b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-26 10:28:00 -0400

    Parkland Mall 1.0  /main.js

--- main.js
+++ main.js
@@ -79,7 +79,8 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             return {
                 dataLoaded: false,
                 show_mobile_menu: false,
-                // menu_items: [
+                menu_items: MenuItems
+                // [
                 //     {
                 //         name: "header.stores",
                 //         href: "/stores",
