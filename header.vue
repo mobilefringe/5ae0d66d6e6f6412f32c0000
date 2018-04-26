425d0d2fa347fcfaa4b8c7117f729ccb35e75b79
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-26 10:21:41 -0400

    Parkland Mall 1.0  /header.vue

---
+++ header.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="sticky"><!--<div :class="{sticky : stickyMenu}">-->
+    				<div class="top_bar">
+    					<div class="site_container">
+    						<div class="row top_bar_wrapper">
+    							<div class="col-sm-6">
+    								<div class="mobile_site_logo visible_phone">
+        							    <router-link to="/"><img :src="property_logo" alt="Property Logo"/></router-link>
+        						    </div>
+    								<div id="home_hours_container" class="hidden_phone" v-if="todays_hours">
+    									<p class="open_now"><span v-if="todays_hours.is_closed == null || !todays_hours.is_closed ">{{$t("header.open_today")}}</span><span v-else>{{$t("header.closed")}}</span> <span v-if="todays_hours.is_closed == null || !todays_hours.is_closed "><span style="margin:0 20px">|</span> {{todays_hours.open_time | moment("h:mma", timezone)}} - {{todays_hours.close_time | moment("h:mma", timezone)}} </span></p>
+    								</div>
+    							</div>
+    							<div class="col-sm-6 hidden_phone text-right">
+    								<div class="header_social">
+    								    <social-links></social-links>
+    								</div>
+    								<router-link id="signup" to="/newsletter">{{$t("header.sign_up")}}</router-link>
+    								<!--<span> <span @click="changeLocale('en-ca')"> en</span> | <span @click="changeLocale('fr-ca')">fr</span></span>-->
+    							</div>
+    							<div id="menu-icon" @click="show_mobile_menu = !show_mobile_menu" :class="{ open: show_mobile_menu}">
+    								<span></span>
+    								<span></span>
+    								<span></span>
+    								<span></span>
+    							</div>
+    							<div class="mobile_nav_container visible_phone">
+    							    <transition name="custom-classes-transition" enter-active-class="animated slideInDown" leave-active-class="animated slideOutUp">
+        								<nav id="mobile_nav" v-show="show_mobile_menu">
+        									<ul>
+        										<div class="mobile_menu_site_logo">
+        											<router-link to="/"><img :src="property_logo" alt="Property Logo"/></router-link>
+        										</div>
+        										<li v-for="item in menu_items" class="menu_item">
+            								        <router-link :to="item.href">{{$t(item.name)}}</router-link>
+            								        <ul v-if="item.sub_menu">
+            								            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
+            								                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank">{{$t(sub_menu.name)}}</a>
+            								                <router-link v-else :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
+            								            </li>
+            										</ul>
+            								    </li>
+        									</ul>
+        									<div class="small_hr"></div>
+        									<div class="tel_num" v-if="property">
+                                                <a :href="'tel:'+property.contact_phone">{{property.contact_phone}}</a>
+                                            </div>
+                                            <div>
+                                               <p style="display:block"> {{property.address1}}</p>
+                                                <p style="display:block">{{property.city}}, {{property.postal_code}} {{property.province_state}}</p>
+                                            </div>
+        									<div class="header_social">
+                                                <social-links></social-links>
+        									</div>
+        									<div class="small_hr"></div>
+        								</nav>
+    								</transition>
+    							</div>
+    						</div>
+    					</div>
+    				</div>
+    				<div class="menu_bar hidden_phone">
+        				<div class="site_container">
+        					<div class="nav_container hidden_phone">
+        						<div class="site_logo">
+        							<router-link to="/"><img :src="property_logo" alt="Property Logo"/></router-link>
+        						</div>
+        						<div class="row top_nav hidden_phone">
+        							<nav id="primary_nav">
+        								<ul>
+        								    <li v-for="item in menu_items" class="menu_item">
+        								        <router-link :to="item.href">{{$t(item.name)}}</router-link>
+        								        <ul v-if="item.sub_menu">
+        								            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
+        								                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank">{{$t(sub_menu.name)}}</a>
+            								            <router-link v-else :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
+        								            </li>
+        										</ul>
+        								    </li>
+        								</ul>
+        							</nav>
+        						</div>
+        					</div>
+        				</div>
+    				</div>
+				</div>
+</template>
