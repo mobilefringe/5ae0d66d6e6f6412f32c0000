b9e9f7b239ecb262164c047b2f7aba105b8fa457
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-03 10:12:18 -0400

    Parkland Mall 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -144,7 +144,7 @@
                     }
                 },
                 shareURL(slug){
-                    var share_url = "http://bonniedoonshoppingcentre.com/promotions/" + slug;
+                    var share_url = "http://parklandmall.ca/promotions/" + slug;
                     return share_url;
                 },
             }
