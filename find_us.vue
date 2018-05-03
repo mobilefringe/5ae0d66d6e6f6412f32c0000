2ce2ff18ccf4b6eda4e6660bc4e4ed41ad46e16d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-03 11:35:37 -0400

    Parkland Mall 1.0  /find_us.vue

---
+++ find_us.vue
@@ -0,0 +1,66 @@
+<template>
+	<div id="find_us_container">
+	    <div  v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')', marginBottom:'0px'}">
+			<div class="site_container">
+				<div class="header_content">
+					<h1>Find Us</h1>
+				</div>
+			</div>
+		</div>  
+		<!-- for some reason if you do not put an outer container div this component template will not render -->
+		<div>
+			<div class="row text-left">
+				<google-map :property="property"></google-map>
+                <div class="text-left site_container padding_tb_30" v-if="currentPage" v-html="currentPage.body"></div>
+					
+			</div>
+			<div class="padding_top_40"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate'], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate) {
+        Vue.use(Meta);
+        Vue.use(VeeValidate);
+        return Vue.component("find-us-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    success_subscribe: false,
+                    currentPage: null,
+                    pageBanner: null
+                }
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.currentPage = response[0].data;
+                    var temp_repo = this.findRepoByName('Contact Us Banner');
+                    if(temp_repo) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    // this.pageBanner = this.findRepoByName('Contact Us Banner').images[0];
+                //   console.log(this.pageBanner); 
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName'
+                ]),
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "	/pages/bonniedoon-contact-us.json"}),this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+            }
+        });
+    });
+</script>
