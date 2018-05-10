4d15ee2ee814b183a55bd002c73cc149e5ae1a27
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-10 12:15:21 -0400

    Parkland Mall 1.1  /social_links.vue

---
+++ social_links.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <a href="https://www.instagram.com/parklandmall" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
+        <a href="https://www.facebook.com/ParklandMall" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
+        <a href="https://www.twitter.com/parklandmallrd" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
+    </div>
+    
+</template>
+<style>
+    .lb-data .lb-number, .lb-nav {
+        display:none!important;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
