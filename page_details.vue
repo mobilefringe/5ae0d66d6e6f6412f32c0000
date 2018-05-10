c96d119bdae599cac42b6ae8687d13d6768962da
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-10 11:38:59 -0400

    Parkland Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -76,7 +76,7 @@
                         this.currentPage = response[0].data;
                         var temp_repo = null;
                         //Add custom banners for indivial pages 
-                        if( _.includes(id, 'gift-cards')) {
+                        if( _.includes(id, 'press-releases')) {
                             temp_repo = this.findRepoByName('Giftcards Banner');
                         }
                         else if ( _.includes(id, 'services')) {
