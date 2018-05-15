ef981cd53b7f8cd0bd1b7b8f3d41ef5d8ed70b33
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-15 14:28:19 -0400

    Parkland Mall 1.1  /events.vue

--- events.vue
+++ events.vue
@@ -171,7 +171,7 @@
                                 'length': 150
                             });
                             if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
-                                value.store.store_front_url_abs = vm.property.default_logo;
+                                value.store.store_front_url_abs = vm.property.default_logo_url;
                             }
                             else if (value.store == null || value.store == undefined) {
                                 value.store = {};
