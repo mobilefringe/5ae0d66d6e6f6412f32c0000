942cff4f1be526700e4c034ef1667fd33a597512
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-24 16:47:36 -0400

    Parkland Mall 1.1  /kidz_club.vue

--- kidz_club.vue
+++ kidz_club.vue
@@ -39,7 +39,6 @@
                             <radio :name="'gender_'+ (randKey+1)" value="female" v-model="child_form_data.gender" :key="randKey+1">Female</radio>
                             <span v-if="genderError" class="form-control-feedback">Please choosed a gender</span>
             			</div>
-						<a @click="incrementChildren()" v-show="!noMoreChildren" style="margin: 10px auto 30px;display: inline-block;"> <i class="fa fa-plus"></i>Add Another Child</a>
 						<div class="form-group ">
 							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('first_name')}">
 								<label class="label" for="first_name">Guardian's First Name <span class="req_star"> *</span></label>
