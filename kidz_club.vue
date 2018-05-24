7aa870b5ac1b1f575054ad19d8bc1684ef179508
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-24 16:55:41 -0400

    Parkland Mall 1.1  /kidz_club.vue

--- kidz_club.vue
+++ kidz_club.vue
@@ -29,7 +29,7 @@
                 				<span v-show="errors.has('child_last_name')" class="form-control-feedback">{{ errors.first('child_last_name') }}</span>
                 			</div>
                 			<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('birthday')}"   style="padding-top: 20px;">
-                				<label class="label" :for="'birthday'+ id_num">Birthday <span class="req_star"> *</span></label>
+                				<label class="label" for="birthday">Birthday <span class="req_star"> *</span></label>
                 				<input v-model="form_data.birthday" class="form-control" v-validate="'required|date_format:MM/DD/YYYY'" :class="{'input': true}" name="birthday" type="text" placeholder="mm/dd/yyyy" data-vv-delay="500" data-vv-as="birthday" :id="birthday">
                 				<span v-show="errors.has('birthday')" class="form-control-feedback">{{ errors.first('birthday') }}</span>
                 			</div>
