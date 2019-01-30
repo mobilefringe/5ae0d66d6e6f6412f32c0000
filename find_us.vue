02b5ef95627af8c7852a918a30d58d2f1e1cb989
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-01-30 12:23:39 -0500

    Parkland Mall 1.1  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="find_us_container">
+	<div id="find_us_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
 	    <div  v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')', marginBottom:'0px'}">
 			<div class="site_container">
 				<div class="header_content">
@@ -7,7 +7,6 @@
 				</div>
 			</div>
 		</div>  
-		<!-- for some reason if you do not put an outer container div this component template will not render -->
 		<div class="site_container">
 			<div class="row text-left" style="margin:30px auto;">
 			    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2440.613977882536!2d-113.81218554887757!3d52.28670976114537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537455b49f248801%3A0xe01229fcdbb6c8f4!2sParkland+Mall!5e0!3m2!1sen!2sca!4v1548868823076" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -17,12 +16,11 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "vue!google_map"], function(Vue, Vuex, moment, GoogleMapAPI) {
+    define(["Vue", "vuex"], function(Vue, Vuex) {
         return Vue.component("find-us-component", {
             template: template, // the variable template will be injected,
             data: function() {
                 return {
-                    success_subscribe: false,
                     currentPage: null,
                     pageBanner: null
                 }
